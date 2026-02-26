73218704d1911f67ff7d2b3ff40896755207b69c
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-02-26 12:41:18 +0500

    destructure the array and then loop each element by map method to  display data

--- expense-stracker/src/components/ExpenseItem.jsx
+++ expense-stracker/src/components/ExpenseItem.jsx
@@ -1,15 +1,21 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-   let {title,day,month,year,expensesAmount}=props
+function ExpenseItem({expenses}) {
+  
   return (
-    <div className='expense-item'>
-      <div className='expense-item__date'>{`${day}-${month}-${year}`}</div>
+    <>
+  {
+    expenses.map((expense,index)=>(
+
+        <div className='expense-item' key={index}>
+      <div className='expense-item__date'>{`${expense.day}-${expense.month}-${expense.year}`}</div>
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>{`${expensesAmount}$`}</div>
+        <h2>{expense.expenseTitle}</h2>
+        <div className='expense-item__price'>{`${expense.expenseAmount}$`}</div>
       </div>
     </div>
+    ))
+  }</>
   );
 }
 
