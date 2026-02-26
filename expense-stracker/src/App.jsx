1225e6e0c8449f065bca1b4fd292ed5a54497c18
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-02-26 12:39:35 +0500

    send array through props of expenseItem

--- expense-stracker/src/App.jsx
+++ expense-stracker/src/App.jsx
@@ -37,11 +37,14 @@ const expenses=[
   return (
     <div>
       <h2>Hello coder</h2>
-      <ExpenseItem title={expenses[0].expenseTitle} month={expenses[0].month} day={expenses[0].day}  year={expenses[0].year} expensesAmount={expenses[0].expenseAmount}></ExpenseItem>
+      {/* <ExpenseItem title={expenses[0].expenseTitle} month={expenses[0].month} day={expenses[0].day}  year={expenses[0].year} expensesAmount={expenses[0].expenseAmount}></ExpenseItem>
        <ExpenseItem title={expenses[1].expenseTitle} month={expenses[1].month} day={expenses[1].day}  year={expenses[1].year} expensesAmount={expenses[1].expenseAmount}></ExpenseItem>
       <ExpenseItem title={expenses[2].expenseTitle} month={expenses[2].month} day={expenses[2].day}  year={expenses[2].year} expensesAmount={expenses[2].expenseAmount}></ExpenseItem>
-     
+      */}
+      <ExpenseItem expenses={expenses}></ExpenseItem>
+      
     </div>
+    
   )
 }
 
