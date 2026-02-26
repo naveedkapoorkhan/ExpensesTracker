ff1fe6be910549e16cc1341dd99e9d62d0743d58
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-02-26 13:24:55 +0500

    Update ExpenseItem component: - Use props destructuring - Map through expenses array - Improve date structure (month, year, day divs)

--- expense-stracker/src/components/ExpenseItem.jsx
+++ expense-stracker/src/components/ExpenseItem.jsx
@@ -1,14 +1,20 @@
 import './ExpenseItem.css';
 
 function ExpenseItem({expenses}) {
-  
+  //props is always an object even if you send it array still will be object 
+
   return (
     <>
+   
   {
     expenses.map((expense,index)=>(
 
         <div className='expense-item' key={index}>
-      <div className='expense-item__date'>{`${expense.day}-${expense.month}-${expense.year}`}</div>
+      <div className='expense-item__date'>
+        <div className="month">{expense.month}</div>
+        <div className="year">{expense.year}</div>
+        <div className="day">{expense.day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{expense.expenseTitle}</h2>
         <div className='expense-item__price'>{`${expense.expenseAmount}$`}</div>
