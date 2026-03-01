54c884e6fd45a9e90a92cffefc87a71c80a9386b
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-01 21:03:44 +0500

    update ExpenseItem.jsx with import ExpenseDescription

--- expense-stracker/src/components/ExpenseItem.jsx
+++ expense-stracker/src/components/ExpenseItem.jsx
@@ -1,7 +1,8 @@
 import ExpenseDate from './ExpenseDate';
+
 import './ExpenseItem.css';
 
-
+import ExpenseDescription from "./ExpenseDescription"
 function ExpenseItem({expenses}) {
   //props is always an object even if you send it array still will be object 
 
@@ -12,14 +13,14 @@ function ExpenseItem({expenses}) {
     expenses.map((expense,index)=>(
         
         <div className='expense-item' key={index}>
-       <ExpenseDate day={expense.day} month={expense.month} year={expense.year}></ExpenseDate>
-      <div className='expense-item__description'>
-        <h2>{expense.expenseTitle}</h2>
-        <div className='expense-item__price'>{`${expense.expenseAmount}$`}</div>
-      </div>
+        <ExpenseDate day={expense.day} month={expense.month} year={expense.year}></ExpenseDate>
+        <ExpenseDescription title={expense.expenseTitle} amount={expense.expenseAmount} ></ExpenseDescription>
+        
     </div>
     ))
-  }</div>
+  }
+  
+  </div>
   );
 }
 
