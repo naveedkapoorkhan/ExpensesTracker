7998277862556a8a5b7701f69e4004b6fbcd4fca
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-02 23:09:14 +0500

    import and use NewExpense.jsx

--- expense-stracker/src/App.jsx
+++ expense-stracker/src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import ExpenseItem from './components/ExpenseItem'
-import expenses from './components/Expenses'
-import Card from './components/Card'
-
+import ExpenseItem from './components/UI/ExpenseItem'
+import expenses from './components/Expenses/Expenses'
+import Card from './components/UI/Card'
+import NewExpense from "./components/NewExpense/NewExpense"
 const App = () => {
    
-
+ 
 
   return (
     <div >
-      <h2>Hello coder</h2>
+     
       {/* <ExpenseItem title={expenses[0].expenseTitle} month={expenses[0].month} day={expenses[0].day}  year={expenses[0].year} expensesAmount={expenses[0].expenseAmount}></ExpenseItem>
        <ExpenseItem title={expenses[1].expenseTitle} month={expenses[1].month} day={expenses[1].day}  year={expenses[1].year} expensesAmount={expenses[1].expenseAmount}></ExpenseItem>
       <ExpenseItem title={expenses[2].expenseTitle} month={expenses[2].month} day={expenses[2].day}  year={expenses[2].year} expensesAmount={expenses[2].expenseAmount}></ExpenseItem>
       */}
       {/* <ExpenseItem expenses={expenses}></ExpenseItem> */}
+      <NewExpense></NewExpense>
       <Card>
       
         <ExpenseItem expenses={expenses}></ExpenseItem>
 
       </Card>
-      
       
     </div>
     
