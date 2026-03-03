bbc1c54dbded99ea4c789dfe644624709f61023b
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-04 00:18:39 +0500

    Updated App.js by props Data get from child component  NewExpenses

--- expense-stracker/src/App.jsx
+++ expense-stracker/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseItem from './components/UI/ExpenseItem'
 import expenses from './components/Expenses/Expenses'
 import Card from './components/UI/Card'
 import NewExpense from "./components/NewExpense/NewExpense"
 const App = () => {
-   
+   let [userExpenses,setUserExpenses]=useState(expenses)
+
+ function newExpenseDataUser(data){
+ 
+ setUserExpenses(previous=>[data,...previous])
+
+ }
  
 
   return (
@@ -15,10 +21,10 @@ const App = () => {
       <ExpenseItem title={expenses[2].expenseTitle} month={expenses[2].month} day={expenses[2].day}  year={expenses[2].year} expensesAmount={expenses[2].expenseAmount}></ExpenseItem>
       */}
       {/* <ExpenseItem expenses={expenses}></ExpenseItem> */}
-      <NewExpense></NewExpense>
+      <NewExpense userDataExpense={newExpenseDataUser} ></NewExpense>
       <Card>
       
-        <ExpenseItem expenses={expenses}></ExpenseItem>
+        <ExpenseItem expenses={userExpenses}></ExpenseItem>
 
       </Card>
       
