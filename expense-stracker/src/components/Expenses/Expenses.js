25b8a2f7b01d21b4201a87154987d51636b9258a
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-04 00:19:30 +0500

    Updated Expenses.jsx and refactor Array objects

--- expense-stracker/src/components/Expenses/Expenses.js
+++ expense-stracker/src/components/Expenses/Expenses.js
@@ -1,55 +1,32 @@
-const expenseDate=new Date()
-    const expenseTitle="wine expenses"
-    const month = expenseDate.toLocaleString('en-US', { month: 'long' });
-  const day = expenseDate.toLocaleString('en-US', { day: '2-digit' });
-  const year = expenseDate.getFullYear();
-const expenseAmount=240000
+// ////const expenseDate=new Date()
+// //     const expenseTitle="wine expenses"
+
+// // const expenseAmount=240000
 
 const expenses=[
   {
-    expenseTitle:"Sports expenses",
-   
-    month:month,
-    day:day,
-    year:year,
-    expenseAmount:200
+    id:1,
+    title:"House Expenses",
+    date:new Date(),
+    amount:20
   }
+
   , {
-    expenseTitle:"HouseHold expenses",
-   
-    month:month,
-    day:day,
-    year:year,
-    expenseAmount:20000
+    
+    id:2,
+    title:"Tuition Expenses",
+    date:new Date(),
+    amount:20
   },
    {
-    expenseTitle:"Transportation expenses",
    
-    month:month,
-    day:day,
-    year:year,
-    expenseAmount:500
+    id:3,
+    title:"Traveling Expenses",
+    date:new Date(),
+    amount:20
   }
 ]
 
-let expense={
-     expenseTitle:"School expenses",
-   
-    month:month,
-    day:day,
-    year:year,
-    expenseAmount:6000
-}
-let expense1={
-     expenseTitle:"Tuition expenses",
-   
-    month:month,
-    day:day,
-    year:year,
-    expenseAmount:2000
-}
-expenses.push(expense)
-expenses.push(expense1)
 
 
 
