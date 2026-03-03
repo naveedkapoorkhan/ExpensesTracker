750e6d67e88098c8b9f586564ca868ae4e745f6b
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-04 00:22:05 +0500

    u[pdated just to apply some date methods

--- expense-stracker/src/components/UI/ExpenseDate.jsx
+++ expense-stracker/src/components/UI/ExpenseDate.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./ExpenseDate.css"
-const ExpenseDate = (expense) => {
-   
+const ExpenseDate = ({date}) => {
+      const month = date.toLocaleString('en-US', { month: 'long' });
+      const day = date.toLocaleString('en-US', { day: '2-digit' });
+      const year = date.getFullYear();
+    
   return (
    <div className='expense-item__date'>
-        <div className="month">{expense.month}</div>
-        <div className="year">{expense.year}</div>
-        <div className="day">{expense.day}</div>
+        <div className="month">{month}</div>
+        <div className="year">{year}</div>
+        <div className="day">{day}</div>
       </div>
   )
 }
