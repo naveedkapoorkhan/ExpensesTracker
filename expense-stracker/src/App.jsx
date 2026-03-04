6bc3300f78816b4cf3692ed2c59d058fb0741621
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-04 22:52:24 +0500

    TotalExpenses.jsx import

--- expense-stracker/src/App.jsx
+++ expense-stracker/src/App.jsx
@@ -3,6 +3,7 @@ import ExpenseItem from './components/UI/ExpenseItem'
 import expenses from './components/Expenses/Expenses'
 import Card from './components/UI/Card'
 import NewExpense from "./components/NewExpense/NewExpense"
+import TotalExpenses from './components/TotalExpenses/TotalExpenses'
 const App = () => {
    let [userExpenses,setUserExpenses]=useState(expenses)
 
@@ -16,17 +17,14 @@ const App = () => {
   return (
     <div >
      
-      {/* <ExpenseItem title={expenses[0].expenseTitle} month={expenses[0].month} day={expenses[0].day}  year={expenses[0].year} expensesAmount={expenses[0].expenseAmount}></ExpenseItem>
-       <ExpenseItem title={expenses[1].expenseTitle} month={expenses[1].month} day={expenses[1].day}  year={expenses[1].year} expensesAmount={expenses[1].expenseAmount}></ExpenseItem>
-      <ExpenseItem title={expenses[2].expenseTitle} month={expenses[2].month} day={expenses[2].day}  year={expenses[2].year} expensesAmount={expenses[2].expenseAmount}></ExpenseItem>
-      */}
-      {/* <ExpenseItem expenses={expenses}></ExpenseItem> */}
+     
       <NewExpense userDataExpense={newExpenseDataUser} ></NewExpense>
       <Card>
       
         <ExpenseItem expenses={userExpenses}></ExpenseItem>
-
+        <TotalExpenses expenses={userExpenses}/>
       </Card>
+  
       
     </div>
     
