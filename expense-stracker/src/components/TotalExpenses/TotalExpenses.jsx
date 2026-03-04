7bec630f0eed3598e7531b2ac078bd42e91ea05c
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-04 22:55:30 +0500

    create TotalExpenses.jsx

---
+++ expense-stracker/src/components/TotalExpenses/TotalExpenses.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./TotalExpenses.css"
+const TotalExpenses = ({expenses}) => {
+    //+expense.amount here + mean a value from input coming string we convert it into number or Number(expense.amount)
+    let totalExpenses=expenses.reduce((sum,expense)=>sum + +expense.amount,0)
+    return (
+    <div className='totalExpense'>
+
+      <div className='totalexpenses-text'>Total Expenses</div>
+      <div className='totalexpenses-value'>{totalExpenses} $</div>
+
+    </div>
+  )
+}
+
+
+export default TotalExpenses
