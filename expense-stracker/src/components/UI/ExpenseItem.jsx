36b0c1d2ecbe9d1fdd4c72b0ec0723a19bcc3868
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-06 16:07:29 +0500

    refactor:add delete and update button and write their logic

--- expense-stracker/src/components/UI/ExpenseItem.jsx
+++ expense-stracker/src/components/UI/ExpenseItem.jsx
@@ -1,19 +1,37 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDescription from "./ExpenseDescription"
+import {useState} from "react"
 function ExpenseItem({expenses}) {
   //props is always an object even if you send it array still will be object 
- 
+  let [items,setItems]=useState(expenses)
+ function deleteItem(id){
+   let newItems = items.filter((item) => {
+  return item.id !== id
+})
+   setItems(newItems)
+ }
+
+ function updateItem(id){
+ let updatedItem= items.map((item)=>{
+   return item.id===id? {...item,title:"Naveed Khan Kapoor",amount:200}:item
+  })
+setItems(updatedItem);
+ }
+
   return (
     <div className='main-expenseItem'>
-   
+   {items.length === 0 && <h2>Sorry No Item Exist</h2>}
   {
-    expenses.map((expense)=>(
+    items.map((expense)=>(
         
         <div className='expense-item' key={expense.id}>
         <ExpenseDate date={expense.date}></ExpenseDate>
         <ExpenseDescription title={expense.title} amount={expense.amount}></ExpenseDescription>
-     
+       <div className="button-expenseItem">
+         <button className="deleteButton"  onClick={()=>{deleteItem(expense.id)}}>Delete</button>
+         <button className="updateButton" onClick={()=>{ updateItem(expense.id)}}>Update</button>
+       </div>
     </div>
     ))
   }
