2aeb56390542806d2beacee1670ce0fd40ffd4a6
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-12 00:50:37 +0500

    add delete router

--- server/routers/ExpenseRoute.js
+++ server/routers/ExpenseRoute.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { AddExpenses, GetExpenses } from "../controllers/ExpensesController.js"
+import { AddExpenses, GetExpenses ,DeleteExpenses} from "../controllers/ExpensesController.js"
 const router=express.Router()
 router.post("/upload",AddExpenses)
 router.get("/getExpenses",GetExpenses)
+router.delete("/delete/:id",DeleteExpenses)
 export default router
