5089d1f38c8dc2ed45c2f5969672254b38642167
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-12 00:49:49 +0500

    Add DeleteExpenses()

--- server/controllers/ExpensesController.js
+++ server/controllers/ExpensesController.js
@@ -9,3 +9,13 @@ export const AddExpenses=(req,res)=>{
 export const GetExpenses=(req,res)=>{
  res.json(Expenses)
 }
+
+export const DeleteExpenses=(req,res)=>{
+   const expenseID =Number(req.params.id)
+  const newExpenses = Expenses.filter((expense)=>expense.id !== expenseID)
+  Expenses.length = 0
+   Expenses.push(...newExpenses)
+
+ 
+  res.json({newExpenses,message:"deleted sucessfully"})
+}
