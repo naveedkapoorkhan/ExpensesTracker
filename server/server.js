b0c812c3591bc21862ba0f94a1e0e2fc1ab3c2ee
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-13 01:20:21 +0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
+import { PORT } from "./configuration/config.js"
 import express from "express"
+ 
 import cors from "cors"
 import addExpense from "./routers/ExpenseRoute.js"
+import dbConnection from "./dbConfig/dbConnection.js"
+
 const app=express()
 app.use(cors())
 app.use(express.json())
 app.use("/expense",addExpense)
-const Port=5000
 
-app.listen(Port,()=>{
-    console.log(`server is running on Port ${Port}`)
+dbConnection()
+app.listen(PORT,()=>{
+    console.log(`server is running on Port ${PORT}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
