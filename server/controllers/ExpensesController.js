8ef00390ee5148b62151448bd662089be154e58b
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-12 22:16:21 +0500

    add updateExpenses method and export

--- server/controllers/ExpensesController.js
+++ server/controllers/ExpensesController.js
@@ -12,10 +12,55 @@ export const GetExpenses=(req,res)=>{
 
 export const DeleteExpenses=(req,res)=>{
    const expenseID =Number(req.params.id)
-  const newExpenses = Expenses.filter((expense)=>expense.id !== expenseID)
-  Expenses.length = 0
+   const newExpenses = Expenses.filter((expense)=>expense.id !== expenseID)
+   Expenses.length = 0
    Expenses.push(...newExpenses)
 
  
-  res.json({newExpenses,message:"deleted sucessfully"})
+   res.json({Expenses,message:"deleted sucessfully"})
+}
+
+// export const UpdateExpenses = (req, res) => {
+
+//    const expenseID = Number(req.params.id)
+//    const editExpense = req.body
+
+//    // remove old expense
+//    const filteredExpenses = Expenses.filter(
+//       (expense) => expense.id !== expenseID
+//    )
+
+//    // add updated expense
+//    const updatedExpense = {
+//       id: expenseID,
+//       ...editExpense
+//    }
+
+//    Expenses.length = 0
+//    Expenses.push(...filteredExpenses, updatedExpense)
+
+//    res.json({
+//       message: "Expense updated successfully",
+//       Expenses
+//    })
+// }
+
+export const UpdateExpenses = (req,res)=>{
+   const expenseID = Number(req.params.id)
+   const editExpense = req.body
+
+   const updatedExpenses = Expenses.map((expense)=>{
+      if(expense.id === expenseID){
+         return {...expense,...editExpense}
+      }
+      return expense
+   })
+
+   Expenses.length = 0
+   Expenses.push(...updatedExpenses)
+
+   res.json({
+      message:"Expense updated successfully",
+      Expenses
+   })
 }
