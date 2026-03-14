2f43b0e7fe73084c555fe3f5b91e02b16ac1f54c
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-15 00:04:10 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,16 +4,20 @@ import express from "express"
 import cors from "cors"
 import addExpense from "./routers/ExpenseRoute.js"
 import dbConnection from "./dbConfig/dbConnection.js"
-
+import authRouter from "./routers/authRouter.js"
 const app=express()
 app.use(cors())
 app.use(express.json())
 app.use("/expense",addExpense)
-
+app.use("/user",authRouter)
 dbConnection()
 app.listen(PORT,()=>{
     console.log(`server is running on Port ${PORT}`)
 })
+
+
+
+
 
 
 
