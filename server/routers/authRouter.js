069103c790ed05f9d6fc991bf7889eeeb2406918
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-03-15 00:05:39 +0500

    create authrouter.js

---
+++ server/routers/authRouter.js
@@ -0,0 +1,8 @@
+import express from "express"
+import { userLogin, userRegister } from "../controllers/authController.js"
+const router=express.Router()
+
+router.post("/register",userRegister)
+router.post("/login",userLogin)
+
+export default router
