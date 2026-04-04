9293b452475e77c5c2469be56afb47d77f2f228d
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2026-04-03 23:37:48 -0700

    Update

--- server/configuration/config.js
+++ server/configuration/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 dotenv.config()
  export const PORT=process.env.PORT
 export const DB_URL=process.env.DB_URL
-
+export const JWT_SECRET=process.env.JWT_SECRET
 if (!DB_URL) {
     console.error("❌ ERROR: DB_URL is missing in .env file!");
 }
