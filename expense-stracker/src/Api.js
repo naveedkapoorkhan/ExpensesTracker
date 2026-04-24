06468acb71b0d09e800486596bca2ef0dac7f059
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2026-04-24 14:07:32 -0700

    Update Api.js

--- expense-stracker/src/Api.js
+++ expense-stracker/src/Api.js
@@ -1,8 +1,11 @@
-// import dotenv from "dotenv"
-// dotenv.config()
-// export const Expense_API_URL =process.env.Expense_API_URL
-// export const USER_API_URL =process.env.USER_API_URL 
+// // import dotenv from "dotenv"
+// // dotenv.config()
+// // export const Expense_API_URL =process.env.Expense_API_URL
+// // export const USER_API_URL =process.env.USER_API_URL 
 
 
-export const Expense_API_URL = process.env.REACT_APP_EXPENSE_API_URL;
-export const USER_API_URL = process.env.REACT_APP_USER_API_URL;
+// export const Expense_API_URL = process.env.REACT_APP_EXPENSE_API_URL;
+// export const USER_API_URL = process.env.REACT_APP_USER_API_URL;
+
+export const Expense_API_URL = import.meta.env.VITE_EXPENSE_API_URL;
+export const USER_API_URL = import.meta.env.VITE_USER_API_URL;
