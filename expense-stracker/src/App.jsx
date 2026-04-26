645f3e18be213ed3efa1499d7c3339a218c6f3d5
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2026-04-26 00:56:29 -0700

    Update App.js add route to card /

--- expense-stracker/src/App.jsx
+++ expense-stracker/src/App.jsx
@@ -1,35 +1,52 @@
-import React, { useState } from 'react'
-import "./App.css"
-import ExpenseItem from './components/UI/ExpenseItem'
-import Card from './components/UI/Card'
-import NewExpense from "./Pages/NewExpense/NewExpense"
-import Navbar from './components/Navbar/Navbar'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Login from './Pages/Login/Login'
-import SignUp from './Pages/SignUp/SignUp'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ExpenseItem from './components/UI/ExpenseItem';
+import Card from './components/UI/Card';
+import NewExpense from "./Pages/NewExpense/NewExpense";
+import Navbar from './components/Navbar/Navbar';
+import { Expense_API_URL } from "./Api"; 
+import Login from './Pages/Login/Login';  
+import SignUp from './Pages/SignUp/SignUp';
 const App = () => {
-   
+  const [expenses, setExpenses] = useState([]);
 
+  // The central function to get data
+  const getExpenses = () => {
+    fetch(`${Expense_API_URL}/getExpenses`)
+      .then(res => res.json())
+      .then(data => {
+        const newFirst = [...data].reverse();
+        setExpenses(newFirst);
+      })
+      .catch(err => console.log(err));
+  };
+
+  // Initial load
+  useEffect(() => {
+    getExpenses();
+  }, []);
 
   return (
-   <BrowserRouter BrowserRouter>
-    <div className='main-app'>
-     
-     <Navbar/>
-     <Routes>
-      <Route path='/addExpense' element={<NewExpense/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      
-     </Routes>
-      {/* <NewExpense/>     */}
-      <Card>      
-        <ExpenseItem ></ExpenseItem>
-      </Card>
-        
-    </div>
-    </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <div className='main-app'>
+        <Navbar />
+        <Routes>
+          {/* Pass getExpenses as a prop so the form can refresh the list */}
+          <Route path='/addExpense' element={<NewExpense onAddSuccess={getExpenses} />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+       
+        <Route path='/' element={
+            <Card>      
+              <ExpenseItem expenses={expenses} refreshData={getExpenses} />
+            </Card>
+          } />
+        </Routes>
 
-export default App
+      
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
