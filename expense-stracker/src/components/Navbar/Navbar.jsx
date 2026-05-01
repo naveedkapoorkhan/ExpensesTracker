057f3f387678f095d71e8dacb3afc4fceef47ade
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2026-05-01 06:52:50 -0700

    add logout functionality

--- expense-stracker/src/components/Navbar/Navbar.jsx
+++ expense-stracker/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  // 1. Check if the user is logged in by looking for the token
+  const isLoggedIn = !!localStorage.getItem("token");
+
+  const handleLogout = () => {
+    // 2. Remove token from localStorage
+    localStorage.removeItem("token");
+    
+    alert("Logged out successfully!");
+    
+    // 3. Redirect user to the login page
+    navigate("/login");
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         {/* Brand/Logo - Optional */}
-        <Link className="navbar-brand" to="/addExpense"  >ExpenseTracker</Link>
-        
+        <Link className="navbar-brand" to={isLoggedIn ? "/" : "/login"}>ExpenseTracker</Link>
 
         {/* Toggle button for mobile view */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -15,18 +29,36 @@ const Navbar = () => {
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/addExpense">Add Expenses</Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/"  >Show list of Expenses</Link>
-              
-            </li>
+            {/* Show these navigation links only if the user is logged in */}
+            {isLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/addExpense">Add Expenses</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Show list of Expenses</Link>
+                </li>
+              </>
+            )}
           </ul>
           
           <div className="d-flex gap-2">
+            {/* OLD CODE:
             <Link className="btn btn-outline-light" to="/login">Login</Link>
             <Link className="btn btn-primary" to="/signup">SignUp</Link>
+            */}
+
+            {/* NEW CODE: Show Logout button OR Login/SignUp buttons based on user status */}
+            {!isLoggedIn ? (
+              <>
+                <Link className="btn btn-outline-light" to="/login">Login</Link>
+                <Link className="btn btn-primary" to="/signup">SignUp</Link>
+              </>
+            ) : (
+              <button className="btn btn-danger" onClick={handleLogout}>
+                Logout
+              </button>
+            )}
           </div>
         </div>
       </div>
