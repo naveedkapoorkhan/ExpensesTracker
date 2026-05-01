28cde41b6795713edd0e9a66dca330ca3c3fc486
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2026-05-01 06:51:58 -0700

    make change to send token

--- expense-stracker/src/App.jsx
+++ expense-stracker/src/App.jsx
@@ -7,11 +7,13 @@ import Navbar from './components/Navbar/Navbar';
 import { Expense_API_URL } from "./Api"; 
 import Login from './Pages/Login/Login';  
 import SignUp from './Pages/SignUp/SignUp';
+
 const App = () => {
   const [expenses, setExpenses] = useState([]);
 
   // The central function to get data
   const getExpenses = () => {
+    /* OLD CODE:
     fetch(`${Expense_API_URL}/getExpenses`)
       .then(res => res.json())
       .then(data => {
@@ -19,6 +21,40 @@ const App = () => {
         setExpenses(newFirst);
       })
       .catch(err => console.log(err));
+    */
+
+    // NEW CODE: Get token from localStorage and send it in headers
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      console.log("User is not logged in");
+      setExpenses([]);
+      return;
+    }
+
+    fetch(`${Expense_API_URL}/getExpenses`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) throw new Error("Unauthorized access or token expired");
+        return res.json();
+      })
+      .then(data => {
+        if (Array.isArray(data)) {
+          const newFirst = [...data].reverse();
+          setExpenses(newFirst);
+        } else {
+          setExpenses([]);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        setExpenses([]);
+      });
   };
 
   // Initial load
@@ -31,19 +67,16 @@ const App = () => {
       <div className='main-app'>
         <Navbar />
         <Routes>
-          {/* Pass getExpenses as a prop so the form can refresh the list */}
           <Route path='/addExpense' element={<NewExpense onAddSuccess={getExpenses} />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
        
-        <Route path='/' element={
+          <Route path='/' element={
             <Card>      
               <ExpenseItem expenses={expenses} refreshData={getExpenses} />
             </Card>
           } />
         </Routes>
-
-      
       </div>
     </BrowserRouter>
   );
