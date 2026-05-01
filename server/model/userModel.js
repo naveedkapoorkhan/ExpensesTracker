fe46d03ef999ce3d16518c8097c958f13173f643
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2026-05-01 06:59:49 -0700

    Update userModel.js

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-const {Schema,model}=mongoose
+const { Schema, model } = mongoose
 
 const userSchema = new Schema({
     email: {
@@ -8,13 +8,13 @@ const userSchema = new Schema({
         unique: true,
         lowercase: true,
         trim: true,
-       },
+    },
     password: {
         type: String,
         required: [true, 'Password is required'],
-      //  minlength: [8, 'Password must be at least 8 characters long'],
-      //  select: false // This prevents the password from being returned in queries by default
+        //  minlength: [8, 'Password must be at least 8 characters long'],
+        //  select: false // This prevents the password from being returned in queries by default
     }
 });
-const User=model("user",userSchema)
+const User = model("user", userSchema)
 export default User
